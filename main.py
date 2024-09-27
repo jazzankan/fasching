52874afefde9ba567c035443cadc829aedf7d87c
anders <anders@webbsallad.se>
2024-09-27 11:11:18 +0200

    Börjat om från början, mer seriöst.

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+import requests
 from bs4 import BeautifulSoup
+
+response = requests.get("https://www.fasching.se/?date=0&artist=all&view=default&c=20")
+fasching_page = response.text
+
+#print(fasching_page)
+
+
 
