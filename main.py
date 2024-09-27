3073fa79a49ca5aa02fecce3d488bafed48d06c9
anders <anders@webbsallad.se>
2024-09-27 15:37:12 +0200

    Skapat lista med konsertnummer.

--- main.py
+++ main.py
@@ -4,7 +4,19 @@ from bs4 import BeautifulSoup
 response = requests.get("https://www.fasching.se/?date=0&artist=all&view=default&c=20")
 fasching_page = response.text
 
-#print(fasching_page)
+soup = BeautifulSoup(fasching_page, "html.parser")
+
+artists_select = soup.find("select", class_="sort__select")
+artists_options = artists_select.find_all("option")
+artists_trimmed = artists_options.pop(0)
+option_values = []
+
+for a_o in artists_options:
+    option_values.append(int(a_o["value"]))
+
+#print(artists_options)
+
+print(option_values)
 
 
 
