e93a9c21314d1fc252c3d2838582d647db0cdb44
anders <anders@webbsallad.se>
2024-09-28 11:26:48 +0200

    Klar - tror jag.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 response = requests.get("https://www.fasching.se/?date=0&artist=all&view=default&c=20")
 fasching_page = response.text
@@ -8,15 +9,28 @@ soup = BeautifulSoup(fasching_page, "html.parser")
 
 artists_select = soup.find("select", class_="sort__select")
 artists_options = artists_select.find_all("option")
-artists_trimmed = artists_options.pop(0)
+artists_options.pop(0)
 option_values = []
 
 for a_o in artists_options:
     option_values.append(int(a_o["value"]))
 
-#print(artists_options)
+with open('valuefile.json', 'r') as f:
+    old_values_list = json.load(f)
 
-print(option_values)
+print('\nNya konserter:')
+for o in option_values:
+    if o not in old_values_list:
+        for art_opt in artists_options:
+            if art_opt['value'] == str(o):
+                print(art_opt.get_text())
+
+
+
+#with open('valuefile.json', 'w') as f:
+    #json.dump(option_values,f)
+
+#print(option_values)
 
 
 
